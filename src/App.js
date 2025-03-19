dcf87dc7bcd5aee3fb8ece77ada99554c8d84275
Maiken <maikenjed@hotmail.com>
2025-03-19 13:22:19 +0100

    Legge til header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <Header />
             {/* <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
