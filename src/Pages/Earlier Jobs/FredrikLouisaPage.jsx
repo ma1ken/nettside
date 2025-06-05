43b456b01c322a32229df79fc60d4fb54112c8f4
Maiken Edvardsen <maikenjed@hotmail.com>
2025-06-05 03:06:11 +0200

    I fixed a layout for ikeapage and fredrik og louisa. I finished the ikeapage, only fredrik og louisa remaining

--- src/Pages/Earlier Jobs/FredrikLouisaPage.jsx
+++ src/Pages/Earlier Jobs/FredrikLouisaPage.jsx
@@ -1,12 +1,25 @@
-import "../../Styles/Pages.css"
+import "../../Styles/Pages.css";
+import fredrikoglouisaLogo from "../../Images/fredrikoglouisa-logo.jpg";
 
 export default function FredrikLouisaPage() {
-    return (
-        <div>
-            <h1>Fredrik & Louisa</h1>
-            <h2>My third official job</h2> 
-            <p>more information coming asap..</p>       
+  return (
+    <div className="content-container">
+      <div className="text-image-section">
+        <div className="text">
+          <h1>
+            Fredrik og Louisa
+            <br />
+            Coming soon...
+          </h1>
+          <h2></h2>
+          <p></p>
         </div>
-
-    )
-};
+        <div className="image">
+          <img src={fredrikoglouisaLogo} alt="Fredrik og Louisa Logo" />
+        </div>
+      </div>
+      <p></p>
+      <div></div>
+    </div>
+  );
+}
