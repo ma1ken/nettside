a06a17d8ddf4f858de0b3ada1a3b82a00cb3ba6b
Maiken Edvardsen <maikenjed@hotmail.com>
2025-06-06 19:21:29 +0200

    animation page complete, can add more infro later

--- src/Pages/Earlier Jobs/FredrikLouisaPage.jsx
+++ src/Pages/Earlier Jobs/FredrikLouisaPage.jsx
@@ -2,19 +2,6 @@ import "../../Styles/Pages.css";
 import fredrikoglouisaLogo from "../../Images/fredrikoglouisa-logo.jpg";
 
 export default function FredrikLouisaPage() {
-<<<<<<< HEAD
-  return (
-    <div className="content-container">
-      <div className="text-image-section">
-        <div className="text">
-          <h1>
-            Fredrik og Louisa
-            <br />
-            Coming soon...
-          </h1>
-        
-          <p></p>
-=======
     return (
         <div className="content-container">
             <div className="text-image-section">
@@ -36,7 +23,6 @@ export default function FredrikLouisaPage() {
             </div>
             <p></p>
             <div></div>
->>>>>>> f08a25752a223f3aa32f5fe8b66c398a7b2989c7
         </div>
     );
 }
