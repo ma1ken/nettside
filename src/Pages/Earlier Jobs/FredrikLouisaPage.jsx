65316e7d736444ad8cded6310cd019d5a09bab75
Maiken Edvardsen <maikenjed@hotmail.com>
2025-08-31 01:29:16 +0200

    more info about fredrik and louisa, i would say im about one third done.

--- src/Pages/Earlier Jobs/FredrikLouisaPage.jsx
+++ src/Pages/Earlier Jobs/FredrikLouisaPage.jsx
@@ -9,20 +9,25 @@ export default function FredrikLouisaPage() {
     <div className="content-container">
       <div className="text-image-section">
         <div className="text">
-          <h1>
-            Fredrik og Louisa
-            <br />
-            Coming soon...
-          </h1>
+          <h1>Fredrik og Louisa</h1>
 
-          <p></p>
+          <p>
+            My third job that I started working at after The Bodyshop. This is
+            the job where I've gained most valueable experience that I can use
+            in other jobs, and I learned to connect my UX designer skills with
+            my work.
+            <br />
+            <br />I started working here three months before I started my
+            bachelors degree at Kristiania. This was a department store, where
+            you usually represent a brand. I had the honor to represent Molton
+            Brown, L:A Bruket, Kiehl's Cosmetics and a few other brands. You
+            work at the whole store, so you have to guide the costumers through
+            all brands and not only your own.
+          </p>
         </div>
         <div className="image">
           <img src={fredrikoglouisaLogo} alt="Fredrik og Louisa Logo" />
         </div>
-      </div>
-      <div>
-        <h2>What is Fredrik & Louisa?</h2>
       </div>
 
       <div className="text-image-section">
@@ -31,7 +36,20 @@ export default function FredrikLouisaPage() {
         </div>
         <div className="text">
           <h2>Molton Brown 20% Ambassador</h2>
-          <p></p>
+          <p>
+            A brand that I was very happy with, and a good jump from The Body
+            Shop. I quickly learnt the products and routines and was very happy
+            and proud to sell these products, as their values aligned alot with
+            The Body Shop and mine.
+            <br />
+            <br />
+            The first Counter I worked at. I shared the counter with another
+            colleage, but I had the main responsibility. We were both students
+            and worked two times a week each to guide costumers and other
+            co-workers through the brand. I had direct contact with the brand
+            from sweden and followed their routines and structure with
+            representation and administrative work.
+          </p>
         </div>
       </div>
 
@@ -55,6 +73,10 @@ export default function FredrikLouisaPage() {
         <div className="image">
           <img src={kiehlsLogo} alt="Kiels Logo." />
         </div>
+      </div>
+
+      <div>
+        <h1>SKills</h1>
       </div>
 
       <div>
