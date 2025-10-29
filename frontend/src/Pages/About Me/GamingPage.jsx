d72322927f66f63cac7a338843fd0ac349bdd844
Maiken Edvardsen <maikenjed@hotmail.com>
2025-10-29 15:46:02 +0100

    teszt

--- frontend/src/Pages/About Me/GamingPage.jsx
+++ frontend/src/Pages/About Me/GamingPage.jsx
@@ -3,7 +3,7 @@ import "../../Styles/Pages.css";
 export default function GamingPage() {
     return(
         <div>
-            <h1>Hi dette er en test 12233</h1>
+            <h1>Hi dette er en test 122233</h1>
         </div>
     );
 }
