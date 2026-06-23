5148825ced21cdf2f028ffdc42f14384d14d0148
Maiken Edvardsen <maikenjed@hotmail.com>
2026-06-23 21:47:40 +0200

    Added introduction and img to unimate page

--- frontend/src/Pages/My Work/UniMatePage.jsx
+++ frontend/src/Pages/My Work/UniMatePage.jsx
@@ -1,11 +1,26 @@
 import "../../Styles/Pages.css";
-
+import unimateLogo from "../../Images/unimateLogo.png";
 
 export default function UniMatePage() {
   return (
-    <div className="page">
-      <h1>Unimate Page</h1>
-      <p>This is the Unimate page.</p>
+    <div className="content-container">
+      <div className="text-image-section">
+        <div className="text">
+          <h1>Hi, I'm Maiken!</h1>
+          <p>
+            A mobile app concept I designed for my DS4800 Innovation and
+            Prototyping exam back in 2023. It’s built to cure campus app fatigue
+            and student loneliness by killing the high interaction cost of
+            fragmented university channels and combining them with hobby-driven
+            networking, helping students manage their crazy academic schedule
+            while organically finding their crowd from day one.
+            <h2>GRADE: B</h2>
+          </p>
+        </div>
+        <div className="image, image-crop">
+          <img src={unimateLogo} alt="AI generated Logo of the uniMate app" />
+        </div>
+      </div>
     </div>
   );
 }
